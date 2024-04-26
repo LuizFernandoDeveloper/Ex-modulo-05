98b69e659686e8674e304c29fc71d630a86e5601
LuizFernandoDeveloper <luiz.fernando.developer@outlook.com>
2024-04-26 13:34:58 -0300

    Add javadoc

--- src/main/code/model/Calculator.java
+++ src/main/code/model/Calculator.java
@@ -1,19 +1,50 @@
 package code.model;
 
 public class Calculator {
-
+    /**
+     * Method for calculating addition
+     *
+     * @param n1 numerator
+     * @param n2 denominator
+     * @return n1 + n2
+     *
+     * **/
     public static  int toAdd(int n1, int n2){
         return n1 + n2;
     }
 
+    /**
+     * Method for calculating subtract
+     *
+     * @param n1 numerator
+     * @param n2 denominator
+     * @return n1 - n2
+     *
+     * **/
     public static int subtract(int n1, int n2){
         return n1 - n2;
     }
 
+    /**
+     * Method for calculating multiply
+     *
+     * @param n1 numerator
+     * @param n2 denominator
+     * @return n1 X n2
+     *
+     * **/
     public static int multiply(int n1, int n2){
         return n1 * n2;
     }
 
+    /**
+     * Method for calculating divide
+     *
+     * @param n1 numerator
+     * @param n2 denominator
+     * @return n1 /  n2
+     *
+     * **/
     public static int divide(int n1, int n2){
 
         if(n1==0 || n2==0){
